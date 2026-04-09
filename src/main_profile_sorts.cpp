1d08e1ea7b9b0df48659b129ea09de63aaf64afd
dmsavkov <dmitry_savkov@mail.ru>
2026-04-09 17:00:41 +0300

    upd: applied astyle

--- src/main_profile_sorts.cpp
+++ src/main_profile_sorts.cpp
@@ -2,33 +2,37 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::cout << "Available dtypes: ";
-  for (const auto &dt : available_dtypes) {
-    std::cout << dt << " ";
-  }
-  std::cout << std::endl;
+int main()
+{
+    std::cout << "Available dtypes: ";
+    for (const auto &dt : available_dtypes)
+    {
+        std::cout << dt << " ";
+    }
+    std::cout << std::endl;
 
-  std::cout << "Available algos: ";
-  for (const auto &algo : available_algos) {
-    std::cout << algo << " ";
-  }
-  std::cout << std::endl;
+    std::cout << "Available algos: ";
+    for (const auto &algo : available_algos)
+    {
+        std::cout << algo << " ";
+    }
+    std::cout << std::endl;
 
-  std::filesystem::path output_path = "../build/profiling_results.csv";
-  if (output_path.has_parent_path()) {
-    std::filesystem::create_directories(output_path.parent_path());
-  }
+    std::filesystem::path output_path = "../build/profiling_results.csv";
+    if (output_path.has_parent_path())
+    {
+        std::filesystem::create_directories(output_path.parent_path());
+    }
 
-  ProfileMap MAP;
-  REGISTER(MAP, "insertion", insertion_sort);
-  REGISTER(MAP, "heap", heap_sort);
+    ProfileMap MAP;
+    REGISTER(MAP, "insertion", insertion_sort);
+    REGISTER(MAP, "heap", heap_sort);
 
-  std::vector<size_t> sizes = {10, 100, 5000, 50000};
-  int iters = 20;
+    std::vector<size_t> sizes = {10, 100, 5000, 50000};
+    int iters = 20;
 
-  orchestrate(available_dtypes, available_algos, sizes, iters, output_path,
-              MAP);
+    orchestrate(available_dtypes, available_algos, sizes, iters, output_path,
+                MAP);
 
-  return 0;
+    return 0;
 }
--- src/profiling.cpp
+++ src/profiling.cpp
@@ -8,24 +8,28 @@ void orchestrate(const std::vector<std::string>& dtypes,
                  const std::vector<size_t>& sizes,
                  int iters,
                  std::filesystem::path output_csv_path,
-                 ProfileMap& MAP) 
+                 ProfileMap& MAP)
 {
     std::ofstream output_file(output_csv_path);
     output_file << "Algorithm,Data Type,N Iters,Array Size,Swaps Mean,Swaps Var,Comps Mean,Comps Var,Time Mean,Time Var\n";
 
-    for (const auto& str_dtype : dtypes) {
-        for (const auto& str_algo : algos) {
+    for (const auto& str_dtype : dtypes)
+    {
+        for (const auto& str_algo : algos)
+        {
             std::string full_str = str_algo + "_" + str_dtype;
 
-            if (MAP.find(full_str) == MAP.end()) {
+            if (MAP.find(full_str) == MAP.end())
+            {
                 std::cerr << "Algorithm " << full_str << " not found in map!\n";
                 continue;
             }
 
-            for (size_t size : sizes) {
+            for (size_t size : sizes)
+            {
                 ProfileRes res = MAP[full_str](size, iters);
                 BenchRes benchmark{res.swaps, res.comps, res.deltas, str_algo, iters, size, str_dtype};
-                
+
                 std::string final_line = benchmark.name_of_algo + "," +
                                          benchmark.data_type + "," +
                                          std::to_string(benchmark.n_iters) + "," +
@@ -36,7 +40,7 @@ void orchestrate(const std::vector<std::string>& dtypes,
                                          std::to_string(benchmark.comps.var) + "," +
                                          std::to_string(benchmark.time.mean) + "," +
                                          std::to_string(benchmark.time.var);
-                
+
                 // std::cout << final_line << std::endl; // optional: output to cout as diagram showed final_line output
                 std::cout << final_line << std::endl;
                 output_file << final_line << "\n";
